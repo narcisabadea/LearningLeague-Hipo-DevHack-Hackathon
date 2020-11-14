c97d8b2d770b2beba6f6dc5aefacbd603ffc53eb
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2020-11-14 10:38:52 +0200

    added video call

--- src/components/ClassroomPage.vue
+++ src/components/ClassroomPage.vue
@@ -3,7 +3,76 @@
     <div class="title">Classroom details</div>
     <div class="content">
       <div class="video-chat">
-        <div class="video">video</div>
+        <div class="video">
+          <div id="auth" v-if="!uid">
+            <p>
+              Enter your username to start video chat (hint: superhero1,
+              superhero2)
+            </p>
+            <form v-on:submit.prevent="authLoginUser">
+              <div class="form-group">
+                <input
+                  name="username"
+                  id="username"
+                  class="form-control"
+                  placeholder="Enter your username"
+                  v-model="username"
+                />
+              </div>
+              <div class="form-group">
+                <button type="submit" class="btn btn-success button-add">
+                  Login
+                  <span v-if="showSpinner" class="fa fa-spin fa-spinner"></span>
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <div class="home" v-if="uid">
+            <div id="nav">
+              <button class="btn btn-success" @click="logoutUser">
+                Logout
+              </button>
+            </div>
+            <div class="form-group">
+              <div class="form-group">
+                <p>
+                  Welcome <b>{{ this.username }}</b
+                  >, your UID is <b>{{ this.uid }}</b> <br />
+                  Enter the receiver Id to start a chat
+                </p>
+                <p v-if="error">
+                  <b class="text-danger"> Receiver ID is required </b>
+                </p>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Enter receiver UID"
+                  v-model="receiver_id"
+                />
+              </div>
+
+              <div v-if="incomingCall">
+                <button class="btn btn-success" @click="acceptCall">
+                  Accept Call
+                </button>
+                <button class="btn btn-success" @click="rejectCall">
+                  Reject Call
+                </button>
+              </div>
+              <div v-else-if="ongoingCall">
+                <button class="btn btn-secondary">Ongoing Call ...</button>
+              </div>
+              <div v-else>
+                <button @click="startVideoChat" class="btn btn-secondary">
+                  Start Call
+                  <span v-if="showSpinner" class="fa fa-spin fa-spinner"></span>
+                </button>
+              </div>
+            </div>
+            <div id="callScreen"></div>
+          </div>
+        </div>
         <v-card class="chat">
           <v-card-text class="old-comments">
             <v-list
@@ -42,9 +111,9 @@
         </v-card>
       </div>
       <div class="files">
-          <v-btn text @click="upload = !upload" class="button-add">
-            <span class="mr-2">Upload</span>
-          </v-btn>
+        <v-btn text @click="upload = !upload" class="button-add">
+          <span class="mr-2">Upload</span>
+        </v-btn>
         <div class="files">
           <vue-dropzone
             v-if="upload"
@@ -139,6 +208,7 @@
 </template>
 
 <script>
+import { CometChat } from "@cometchat-pro/chat";
 import vue2Dropzone from "vue2-dropzone";
 import "vue2-dropzone/dist/vue2Dropzone.min.css";
 import firebase from "../plugins/firebase";
@@ -152,6 +222,8 @@ export default {
       fileDetails: "",
       typeUpload: "",
       inputData: "",
+      VUE_APP_COMMETCHAT_API_KEY: "8a8985b76b57b11c2148a65581e6af67a9314bd0",
+      VUE_APP_COMMETCHAT_APP_ID: "2556919f5f12b69",
       dropzoneOptions: {
         url: "https://httpbin.org/post",
         thumbnailWidth: 100,
@@ -159,6 +231,14 @@ export default {
         maxFiles: 1,
       },
       newComment: "",
+      username: "",
+      showSpinner: false,
+      uid: "",
+      session_id: "",
+      receiver_id: null,
+      error: false,
+      incomingCall: false,
+      ongoingCall: false,
     };
   },
   components: {
@@ -285,10 +365,200 @@ export default {
 
       window.open(item.downloadLink, "_blank");
     },
+    initializeApp() {
+      var appID = this.VUE_APP_COMMETCHAT_APP_ID;
+      CometChat.init(appID).then(
+        () => {
+          console.log("Initialization completed successfully");
+        },
+        (error) => {
+          console.log("Initialization failed with error:", error);
+        }
+      );
+    },
+    authLoginUser() {
+      var apiKey = this.VUE_APP_COMMETCHAT_API_KEY;
+      this.showSpinner = true;
+      CometChat.login(this.username, apiKey).then(
+        () => {
+          this.showSpinner = false;
+          this.getLoggedInUser();
+        },
+        (error) => {
+          this.showSpinner = false;
+          console.log("Login failed with error:", error.code);
+        }
+      );
+    },
+    getLoggedInUser() {
+      CometChat.getLoggedinUser().then(
+        (user) => {
+          this.username = user.name;
+          this.uid = user.uid;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    startVideoChat() {
+      if (!this.receiver_id) this.error = true;
+      this.showSpinner = true;
+      var receiverID = this.receiver_id;
+      var callType = CometChat.CALL_TYPE.VIDEO;
+      var receiverType = CometChat.RECEIVER_TYPE.USER;
+      var call = new CometChat.Call(receiverID, callType, receiverType);
+      CometChat.initiateCall(call).then(
+        (outGoingCall) => {
+          this.showSpinner = false;
+          console.log("Call initiated successfully:", outGoingCall);
+          // perform action on success. Like show your calling screen.
+        },
+        (error) => {
+          console.log("Call initialization failed with exception:", error);
+        }
+      );
+    },
+    acceptCall() {
+      let globalContext = this;
+      this.ongoingCall = true;
+      this.incomingCall = false;
+      var sessionID = this.session_id;
+      CometChat.acceptCall(sessionID).then(
+        (call) => {
+          console.log("Call accepted successfully:", call);
+          console.log("call accepted now....");
+          // start the call using the startCall() method
+          console.log(globalContext.ongoingCall);
+          CometChat.startCall(
+            call.sessionId,
+            document.getElementById("callScreen"),
+            new CometChat.OngoingCallListener({
+              onUserJoined: (user) => {
+                /* Notification received here if another user joins the call. */
+                console.log("User joined call:", user);
+                /* this method can be use to display message or perform any actions if someone joining the call */
+              },
+              onUserLeft: (user) => {
+                /* Notification received here if another user left the call. */
+                console.log("User left call:", user);
+                /* this method can be use to display message or perform any actions if someone leaving the call */
+              },
+              onCallEnded: (call) => {
+                /* Notification received here if current ongoing call is ended. */
+                console.log("Call ended:", call);
+                globalContext.ongoingCall = false;
+                globalContext.incomingCall = false;
+                /* hiding/closing the call screen can be done here. */
+              },
+            })
+          );
+        },
+        (error) => {
+          console.log("Call acceptance failed with error", error);
+          // handle exception
+        }
+      );
+    },
+    rejectCall() {
+      var sessionID = this.session_id;
+      var globalContext = this;
+      var status = CometChat.CALL_STATUS.REJECTED;
+      CometChat.rejectCall(sessionID, status).then(
+        (call) => {
+          console.log("Call rejected successfully", call);
+          globalContext.incomingCall = false;
+          globalContext.ongoingCall = false;
+          globalContext.receiver_id = "";
+        },
+        (error) => {
+          console.log("Call rejection failed with error:", error);
+        }
+      );
+    },
+    logoutUser() {
+      CometChat.logout().then(
+        (success) => {
+          console.log("Logout completed successfully");
+          this.$router.push({ name: "homepage" });
+          console.log(success);
+        },
+        (error) => {
+          //Logout failed with exception
+          console.log("Logout failed with exception:", { error });
+        }
+      );
+    },
+  },
+  created() {
+    this.initializeApp();
+
+    let globalContext = this;
+    var listnerID = "UNIQUE_LISTENER_ID";
+    CometChat.addCallListener(
+      listnerID,
+      new CometChat.CallListener({
+        onIncomingCallReceived(call) {
+          console.log("Incoming call:", call);
+          globalContext.incomingCall = true;
+          globalContext.session_id = call.sessionId;
+        },
+        onOutgoingCallAccepted(call) {
+          console.log("Outgoing call accepted:", call);
+          globalContext.ongoingCall = true;
+          CometChat.startCall(
+            call.sessionId,
+            document.getElementById("callScreen"),
+            new CometChat.OngoingCallListener({
+              onUserJoined: (user) => {
+                /* Notification received here if another user joins the call. */
+                console.log("User joined call:", user);
+                /* this method can be use to display message or perform any actions if someone joining the call */
+              },
+              onUserLeft: (user) => {
+                /* Notification received here if another user left the call. */
+                console.log("User left call:", user);
+                /* this method can be use to display message or perform any actions if someone leaving the call */
+              },
+              onCallEnded: (call) => {
+                globalContext.ongoingCall = false;
+                globalContext.incomingCall = false;
+                /* Notification received here if current ongoing call is ended. */
+                console.log("Call ended:", call);
+                /* hiding/closing the call screen can be done here. */
+              },
+            })
+          );
+          // Outgoing Call Accepted
+        },
+        onOutgoingCallRejected(call) {
+          console.log("Outgoing call rejected:", call);
+          this.incomingCall = false;
+          this.ongoingCall = false;
+          this.receiver_id = "";
+          // Outgoing Call Rejected
+        },
+        onIncomingCallCancelled(call) {
+          console.log("Incoming call calcelled:", call);
+        },
+      })
+    );
   },
 };
 </script>
 <style scoped>
+#auth {
+  width: 600px;
+  margin: 0 auto;
+}
+#callScreen {
+  width: 500px;
+  height: 500px;
+}
+.home {
+  width: 600px;
+  margin: 0 auto;
+}
 .title {
   font-family: "dancingscript" !important;
   font-size: 2rem !important;
@@ -337,6 +607,10 @@ export default {
 }
 .video {
   flex: 1 1 40%;
+  padding-right: 10px;
+      align-self: center;
+    text-align: center;
+    justify-content: center;
 }
 .chat {
   flex: 1 1 40%;
